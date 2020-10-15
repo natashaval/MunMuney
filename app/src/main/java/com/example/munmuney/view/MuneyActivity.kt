f8ccbe38e04c14836375036ce183a5f3c95598b6
Natasha Valentina <natasha.santoso@gdn-commerce.com>
2020-10-15 17:16:00 +0700

    bottom navigation

--- app/src/main/java/com/example/munmuney/view/MuneyActivity.kt
+++ app/src/main/java/com/example/munmuney/view/MuneyActivity.kt
@@ -2,13 +2,23 @@ package com.example.munmuney.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.munmuney.R
 import com.example.munmuney.databinding.ActivityMuneyBinding
 
 class MuneyActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMuneyBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMuneyBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
+  private lateinit var binding: ActivityMuneyBinding
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMuneyBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    setUpBottomNav()
+  }
+
+  private fun setUpBottomNav() {
+    val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_host) as NavHostFragment
+    NavigationUI.setupWithNavController(binding.bnvNavigation, navHostFragment.navController)
+  }
 }
